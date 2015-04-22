9480c078f31c51f5fde06c83807a0076e68a61c7
sclek016 <stewartclelland@gmail.com>
2015-04-22 17:54:30 -0400

    Added alternating uniform matrix exp.

--- matlabmodel/experiments.m
+++ matlabmodel/experiments.m
@@ -11,3 +11,18 @@ b = 1.9;
 World = ones(worldSize,worldSize)*COOPERATOR;
 World(round(worldSize/2), round(worldSize/2)) = DEFECTOR;
 prisonerworld(simName, World, generations, b);
+
+%Experiment: Alternating 1 and 0 throughout the matrix
+simName = 'UniformAlternating';
+World = ones(worldSize,worldSize)*COOPERATOR;
+for m=1:worldSize
+    for n=1:worldSize
+        if 1==mod(m+n,2)
+            World(m,n)=DEFECTOR;          
+        end
+        if 0==mod(m+n,2)
+           World(m,n)=DEFECTOR;
+        end
+    end
+end
+prisonerworld(simName, World, generations, b);
