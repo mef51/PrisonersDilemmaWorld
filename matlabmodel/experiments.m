5f0184dec44ecff40cc447aeb24843e0f2d3b570
Stewart <stewart@debian.t430>
2015-04-23 14:21:34 -0400

    Added seemingly useful Uniform percentage exp

--- matlabmodel/experiments.m
+++ matlabmodel/experiments.m
@@ -2,7 +2,7 @@
 DEFECTOR = 0; % a constant, don't change this lel
 COOPERATOR = 1; % don't change this either lel
 
-worldSize = 199;
+worldSize = 200;
 generations = 200;
 
 % Experiment: 1 Defector in the middle of a World of cooperators 
@@ -28,4 +28,19 @@ for m = 1:worldSize
         end
     end
 end
-%prisonerworld(simName, World, generations, b);
+prisonerworld(simName, World, generations, b);
+
+
+simName = 'Uniform Percentage''
+World = ones(worldSize, worldSize)*DEFECTOR;
+percentage = 0.1; %set percent of cooperators
+final_coop = round(percentage*(worldSize*worldSize));
+sofar_coop=0;
+while (sofar_coop<final_coop)
+    m=randi(worldSize);
+    n=randi(worldSize);
+    if World(m,n)==DEFECTOR
+        World(m,n)=COOPERATOR;
+        sofar_coop=sofar_coop+1;
+    end
+end
