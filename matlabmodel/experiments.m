62e1acd05811a88e28dd897c0603d1c950a3f3c5
mohammed chamma <hms.mohammed@gmail.com>
2015-04-24 01:52:51 -0400

    experiment of a cluster of cooperators in the middle of a bunch of defectors

--- matlabmodel/experiments.m
+++ matlabmodel/experiments.m
@@ -12,6 +12,23 @@ World = ones(worldSize,worldSize)*COOPERATOR;
 World(round(worldSize/2), round(worldSize/2)) = DEFECTOR;
 prisonerworld(simName, World, generations, b);
 
+worldSize = 59;
+generations = 40;
+simName = 'CDoilyExperiment';
+b = 1.8;
+World = ones(worldSize,worldSize)*DEFECTOR;
+World(round(worldSize/2), round(worldSize/2)) = COOPERATOR;
+World(round(worldSize/2), round(worldSize/2)+1) = COOPERATOR;
+World(round(worldSize/2), round(worldSize/2)-1) = COOPERATOR;
+World(round(worldSize/2)+1, round(worldSize/2)) = COOPERATOR;
+World(round(worldSize/2)-1, round(worldSize/2)) = COOPERATOR;
+World(round(worldSize/2)+1, round(worldSize/2)+1) = COOPERATOR;
+World(round(worldSize/2)+1, round(worldSize/2)-1) = COOPERATOR;
+World(round(worldSize/2)-1, round(worldSize/2)+1) = COOPERATOR;
+World(round(worldSize/2)-1, round(worldSize/2)-1) = COOPERATOR;
+prisonerworld(simName, World, generations, b);
+
+
 %Experiment: Alternating 1 and 0 throughout the matrix
 simName = 'UniformAlternating';
 World = ones(worldSize,worldSize)*COOPERATOR;
